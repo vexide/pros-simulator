9d33410f407aa18a7320ce01ee9577de812f0a80
doinkythederp <doinkythederp@icloud.com>
2023-10-16 20:42:48 +0000

    feat: add library mode

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,42 @@
-use std::{thread::sleep, time::Duration};
+use std::collections::HashMap;
+use std::future::Future;
+use std::path::Path;
+use std::sync::Arc;
+use std::time::Duration;
 
-use anyhow::anyhow;
-use wasmtime::{Caller, Engine, Instance, Linker, Module, Store};
+use anyhow::bail;
+use anyhow::{anyhow, Result};
+use host::memory::SharedMemoryExt;
+use host::task::TaskPool;
+use host::thread_local::CallerExt;
+use host::Host;
+use host::InnerHost;
+use host::ResultExt;
+use tokio::sync::Mutex;
+use tokio::time::sleep;
+use wasmtime::*;
 
-use crate::host::{thread_local::CallerExt, ErrnoExt, Host, ResultExt};
+pub mod host;
 
-pub async fn create_linker(
-    engine: &Engine,
-    module: &Module,
-) -> anyhow::Result<(Instance, Store<Host>)> {
-    let mut linker = Linker::<Host>::new(engine);
-    let mut store = Store::new(engine, Host::default());
+pub async fn simulate(robot_code: &Path) -> Result<()> {
+    let engine = Engine::new(
+        Config::new()
+            .async_support(true)
+            .wasm_threads(true)
+            .debug_info(true)
+            .wasm_backtrace_details(WasmBacktraceDetails::Enable),
+    )
+    .unwrap();
+    let shared_memory = SharedMemory::new(&engine, MemoryType::shared(18, 16384))?;
+    let host = Arc::new(Mutex::new(InnerHost::new(
+        engine.clone(),
+        shared_memory.clone(),
+    )));
 
+    let mut linker = Linker::<Host>::new(&engine);
+    let mut store = Store::new(&engine, host.clone());
+
+    linker.define(&mut store, "env", "memory", shared_memory)?;
     linker.func_wrap0_async("env", "lcd_initialize", |mut caller: Caller<'_, Host>| {
         Box::new(async move {
             let mut host = caller.data_mut().lock().await;
@@ -27,15 +52,10 @@ pub async fn create_linker(
         "lcd_set_text",
         |mut caller: Caller<'_, Host>, line: i32, text_ptr: u32| {
             Box::new(async move {
-                let memory = caller.data_mut().lock().await.memory.unwrap();
-                let (data, host) = memory.data_and_store_mut(&mut caller);
-                let text = data
-                    .get(text_ptr as usize..)
-                    .and_then(|arr| arr.iter().position(|&x| x == 0))
-                    .and_then(|len| std::str::from_utf8(&data[text_ptr as usize..][..len]).ok())
-                    .ok_or_else(|| anyhow!("invalid UTF-8 string"))?;
-
-                let res = host.lock().await.lcd.set_line(line, text);
+                let mut data = caller.data_mut().lock().await;
+                let text = data.memory.read_c_str(text_ptr)?;
+                let res = data.lcd.set_line(line, &text);
+                drop(data);
                 Ok(u32::from(res.use_errno(&mut caller).await))
             })
         },
@@ -81,7 +101,9 @@ pub async fn create_linker(
         |mut caller: Caller<'_, Host>, task_handle: u32, storage_index: i32| {
             Box::new(async move {
                 let storage = caller.task_storage(task_handle).await;
-                Ok(storage.get_address(storage_index))
+                let data = caller.data_mut().lock().await;
+                let memory = data.memory.clone();
+                Ok(storage.get(memory, storage_index))
             })
         },
     )?;
@@ -89,13 +111,13 @@ pub async fn create_linker(
     linker.func_wrap3_async(
         "env",
         "vTaskSetThreadLocalStoragePointer",
-        |mut caller: Caller<'_, Host>, task_handle: u32, storage_index: i32, address: u32| {
+        |mut caller: Caller<'_, Host>, task_handle: u32, storage_index: i32, value: u32| {
             Box::new(async move {
                 let mut storage = caller.task_storage(task_handle).await;
                 let data = caller.data_mut().lock().await;
-                let memory = data.memory.unwrap();
+                let memory = data.memory.clone();
                 drop(data);
-                storage.set_address(&mut caller, memory, storage_index, address)
+                storage.set(memory, storage_index, value)
             })
         },
     )?;
@@ -109,13 +131,28 @@ pub async fn create_linker(
 
     linker.func_wrap1_async("env", "delay", |_caller: Caller<'_, Host>, millis: u32| {
         Box::new(async move {
-            sleep(Duration::from_millis(millis.into()));
+            sleep(Duration::from_millis(millis.into())).await;
             Ok(())
         })
     })?;
 
     linker.func_wrap0_async("env", "__errno", |mut caller: Caller<'_, Host>| {
-        Box::new(async move { caller.errno_address().await })
+        Box::new(async move {
+            let data = caller.data_mut().lock().await;
+            let current_task = data.tasks.current();
+            drop(data);
+            let errno = current_task.lock().await.errno(&mut caller).await;
+            Ok(errno.address())
+        })
+    })?;
+
+    linker.func_wrap0_async("env", "millis", |mut caller: Caller<'_, Host>| {
+        Box::new(async move {
+            let data = caller.data_mut().lock().await;
+            let start_time = data.start_time;
+            drop(data);
+            Ok(start_time.elapsed().as_millis() as u32)
+        })
     })?;
 
     linker.func_wrap(
@@ -126,5 +163,18 @@ pub async fn create_linker(
         },
     )?;
 
-    Ok((linker.instantiate_async(&mut store, module).await?, store))
+    // Instantiate our module with the imports we've created, and run it.
+    let module = Module::from_file(&engine, robot_code)?;
+
+    let instance = linker.instantiate_async(&mut store, &module).await?;
+
+    // Like before, we can get the run function and execute it.
+    let initialize = instance.get_typed_func::<(), ()>(&mut store, "initialize")?;
+    {
+        let mut host = host.lock().await;
+        host.tasks.spawn(instance, store, initialize);
+    }
+    TaskPool::run_to_completion(&host).await;
+
+    Ok(())
 }
